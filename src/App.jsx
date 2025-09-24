7c59fc4ef2380338b3cb016b2ccd79095e002d3f
DarKouto <danielcouto9@live.com>
2025-09-24 15:05:07 +0100

    updated css and theme.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import Contactos from './components/Contactos';
 import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
@@ -5,10 +7,12 @@ import HeroSection from './components/HeroSection';
 function App() {
   return (
     <>
+<ThemeProvider theme={theme}>
     <HeroSection />
     <div id="content-start"></div>
     <Contactos />
     <Footer />
+</ThemeProvider>
     </>
   )
 }
