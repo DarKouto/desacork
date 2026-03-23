0374b67f025270252eb01505e19c80d58074bfa7
DarKouto <danielcouto9@live.com>
2026-03-23 09:27:33 +0000

    AppBar.vue done

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,12 +1,61 @@
 <script setup>
+  import { ref } from 'vue';
+  const drawer = ref(false);
+  const navItems = [
+    { title: 'Home', to: '/' },
+    { title: 'Sobre Nós', to: '/sobre-nos' },
+    { title: 'Contactos', to: '/contactos' },
+  ];
+  const closeDrawer = () => {
+    return drawer.value = false;
+  };
 </script>
 
 <template>
-  <v-btn to="/" text="Home" color="primary"></v-btn>
-  <v-btn to="/sobre-nos" text="Sobre Nós" color="primary"></v-btn>
-  <v-btn to="/contactos" text="Contactos" color="primary"></v-btn>
+  <v-app-bar color="primary" elevation="2">
+    <v-app-bar-nav-icon 
+      class="hidden-md-and-up" 
+      @click="drawer = !drawer"
+    ></v-app-bar-nav-icon>
+
+    <v-app-bar-title class="font-weight-bold">
+      Desacork
+    </v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <div class="hidden-sm-and-down">
+      <v-btn 
+        v-for="item in navItems" 
+        :key="item.title" 
+        :to="item.to" 
+        variant="text"
+      >
+        {{ item.title }}
+      </v-btn>
+    </div>
+  </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    location="left"
+    temporary
+  >
+    <v-list>
+      <v-list-item
+        v-for="item in navItems"
+        :key="item.title"
+        :to="item.to"
+        @click="closeDrawer"
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <style scoped lang="scss">
- /* */
+.v-app-bar-title {
+  cursor: pointer;
+}
 </style>
