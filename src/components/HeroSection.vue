2c9ff98d14eb38ce7c1fcefc8803c8ef0745980f
DarKouto <danielcouto9@live.com>
2026-03-23 09:04:58 +0000

    added .env, fundo.jpg and herosection css test

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -4,15 +4,22 @@
 </script>
 
 <template>
-  <h2>Este é o components/HeroSection.vue</h2>
-  <h1>Desacork.com</h1>
-  <p>Com Vue e Vuetify</p>
-  <p>{{ texto }}</p>
-  <hr/>
+  <div class="fundo">
+    <h2>Este é o components/HeroSection.vue</h2>
+    <h1>Desacork.com</h1>
+    <p>Com Vue e Vuetify</p>
+    <p>{{ texto }}</p>
+    <hr/>
+  </div>
 </template>
 
 <style scoped lang="scss">
   h2 {
     color: green;
+  }
+  .fundo {
+    background-image: url("/src/images/fundo.jpg");
+    height: 800px;
+    width: auto;
   }
 </style>
